ded1b1261716c9b73dce33f1622f7dd82a5a9cf1
wagner <wagnersoarescamargo@gmail.com>
2018-06-01 00:22:26 -0300

    ajuste classe email

--- waterbridge/src/br/com/waterbridge/auxiliar/Email.java
+++ waterbridge/src/br/com/waterbridge/auxiliar/Email.java
@@ -20,6 +20,7 @@ public class Email {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
