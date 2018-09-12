483cc39ddfcf2f563c9d9bcbff755238b48911af
wagner <wagnersoarescamargo@gmail.com>
2018-09-12 12:54:00 -0300

    mapa dispositivos

--- waterbridge/src/br/com/waterbridge/bo/MapaConsumoPressaoBO.java
+++ waterbridge/src/br/com/waterbridge/bo/MapaConsumoPressaoBO.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import br.com.waterbridge.connection.ConnectionFactory;
+import br.com.waterbridge.modelo.User;
 import br.com.waterbridge.reldao.RelMapaConsumoPressaoDAO;
 import br.com.waterbridge.relmodelo.RelMapaConsumoPressao;
 
@@ -31,18 +32,54 @@ public class MapaConsumoPressaoBO extends HttpServlet {
                    
 		if (req.getParameter("acao") != null && req.getParameter("acao").equals("1")) {//ENTRA NA MAPA
 			
-			Connection connection = null;
+			User user = (User) req.getSession().getAttribute("user");
+			Connection connection = null;			
 			String sql = "";
 			
 			try {
 				
 				connection = ConnectionFactory.getConnection();
-				
-				sql +=
-	            "LEFT JOIN VW_ID_CONSUMOMAX " +
-                "ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " +
-                "WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " ;
-				
+	
+				if(user.getPerfil().getIdPerfil().longValue() == 1) {//PROGRAMADOR
+					sql +=
+		            "LEFT JOIN VW_ID_CONSUMOMAX " +
+	                "ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " +
+	                "WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " ;
+				}
+				else if(user.getPerfil().getIdPerfil().longValue() == 2) {//GERENCIAL
+					sql +=
+		            "LEFT JOIN VW_ID_CONSUMOMAX " +
+	                "ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " +
+	                "WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " ;
+				}
+				else if(user.getPerfil().getIdPerfil().longValue() == 3) {//REPRESENTANTE
+					sql +=				
+					"LEFT JOIN VW_ID_CONSUMOMAX " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " + 
+					"LEFT JOIN VW_USEREMPRESA " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_EMPRESA = VW_USEREMPRESA.ID_EMPRESA " +
+					"WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " +
+					"AND       VW_USEREMPRESA.ID_USER = " + user.getIdUser() + " " ;
+				}
+				else if(user.getPerfil().getIdPerfil().longValue() == 4) {//ADMINISTRADOR LOCAL
+					sql +=
+					"LEFT JOIN VW_ID_CONSUMOMAX " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " + 
+					"LEFT JOIN VW_USERCONDOMINIO " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_CONDOMINIO = VW_USERCONDOMINIO.ID_CONDOMINIO " +
+					"WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " +
+					"AND       VW_USERCONDOMINIO.ID_USER = " + user.getIdUser() + " " ;
+				}
+				else if(user.getPerfil().getIdPerfil().longValue() == 5) {//CONSUMIDOR
+					sql +=
+					"LEFT JOIN VW_ID_CONSUMOMAX " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_CONSUMO = VW_ID_CONSUMOMAX.ID_CONSUMO " + 
+					"LEFT JOIN VW_USERMEDIDOR " +
+					"ON        VW_MAPACONSUMOPRESSAO.ID_MEDIDOR = VW_USERMEDIDOR.ID_MEDIDOR " +
+					"WHERE     VW_ID_CONSUMOMAX.ID_CONSUMO IS NOT NULL " +
+					"AND       VW_USERMEDIDOR.ID_USER = " + user.getIdUser() + " " ;
+				}
+
 				RelMapaConsumoPressaoDAO relMapaConsumoPressaoDAO = new RelMapaConsumoPressaoDAO(connection);
 				List<RelMapaConsumoPressao> listRelMapaConsumoPressao = relMapaConsumoPressaoDAO.listar(sql);
 				
